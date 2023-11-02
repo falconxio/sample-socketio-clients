3b220756d508044522362761735685bca68d5b70
Rohit Chauhan <99669658+chrohi@users.noreply.github.com>
2023-11-02 19:23:41 +0530

    Update ws_client.py

--- websocket/ws_client.py
+++ websocket/ws_client.py
@@ -1,12 +1,14 @@
-import websocket
-from marshmallow import fields
-from marshmallow.validate import Range
 import base64
 import hashlib
 import hmac
-import time
 import json
+import time
 from pprint import pprint
+
+import websocket
+from marshmallow import fields
+from marshmallow.validate import Range
+
 
 class ConnectionOpts:
     retry_on_error = fields.Boolean(default=False)
@@ -15,10 +17,10 @@ class ConnectionOpts:
     log_streams = fields.Boolean(default=False)
 
     def __init__(self,
-                 retry_on_error = False,
-                 num_retries = None,
-                 retry_delay = None,
-                 log_streams = False,
+                 retry_on_error=False,
+                 num_retries=None,
+                 retry_delay=None,
+                 log_streams=False,
                  ) -> None:
         self.retry_on_error = retry_on_error
         self.num_retries = num_retries
@@ -42,7 +44,7 @@ class FalconXWSClient:
     def __init__(self,
                  host: str = None,
                  path: str = "/",
-                 ssl:bool = False,
+                 ssl: bool = False,
                  api_key: str = None,
                  secret: str = None,
                  passphrase: str = None,
@@ -58,7 +60,6 @@ class FalconXWSClient:
 
     def get_signature(self, timestamp, path):
         method = "GET"
-
         message = "".join([timestamp, method, path, ""])
         hmac_key = base64.b64decode(self.secret)
         signature = hmac.new(hmac_key, message.encode(), hashlib.sha256)
@@ -70,41 +71,67 @@ class FalconXWSClient:
         def on_message(ws, message):
             # print(message)
             data = json.loads(message)
-            if data["event"] == "auth_response" and data["status"] == "success":
-                self.authenticated = True
-                self.susbcribe()
-            elif data["event"] == "stream" and data["status"] == "success":
-                pprint(data["body"])
+            if data["event"] == "auth_response":
+                if data["status"] == "success":
+                    print("Authentication successful.")  # noqa
+                    self.authenticated = True
+                    self.fetch_data("max_levels")
+                    self.fetch_data("allowed_markets")
+                    self.fetch_data("max_connections")
+                else:
+                    print("Authentication Unsuccessful. Rsponse: ", data)  # noqa
+            if data["event"] == "subscribe_response":
+                if data["status"] == "success":
+                    print("Subscription successful. Response: ", data)  # noqa
+                else:
+                    print("Subscription failed. Response: ", data)  # noqa
+            if data["event"] == "unsubscribe_response":
+                if data["status"] == "success":
+                    print("UnSubscription successful. Response: ", data)  # noqa
+                else:
+                    print("UnSubscribe failed. Response: ", data)  # noqa
+            if data["event"] == "data_response":
+                if data["status"] == "success":
+                    print("Data Response: ", data)  # noqa
+                else:
+                    print("Data request error: ", data)  # noqa
+            if data["event"] == "error_response":
+                print("Error Response: ", data)  # noqa
+            elif data["event"] == "stream":
+                if data["status"] == "success":
+                    pprint(data)  # noqa
+                else:
+                    print("Error in stream: ", data)  # noqa
 
         def on_error(ws, error):
-            print("Error : ", error)
+            print("Error : ", error)  # noqa
 
         def on_close(ws, close_status_code, close_msg):
-            print("### closed ###")
+            print("### closed ###")  # noqa
 
         def on_open(ws):
-            print("Opened connection")
+            print("Opened connection")  # noqa
             self.authenticate()
 
         # websocket.enableTrace(True)
-        self.conn = websocket.WebSocketApp(self.host+self.path,
-                                on_open=on_open,
-                                on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close)
+        self.conn = websocket.WebSocketApp(self.host + self.path,
+                                           on_open=on_open,
+                                           on_message=on_message,
+                                           on_error=on_error,
+                                           on_close=on_close)
         self.conn.run_forever(reconnect=5)
-    
+
     def authenticate(self):
         timestamp = int(time.time())
         signature = self.get_signature(str(timestamp), self.path)
 
         req = {
-          "action":     "auth",
-          "api_key":    self.api_key,
-          "passphrase": self.passphrase,
-          "sign":       signature,
-          "timestamp":  timestamp,
-          "request_id": "my_request"
+            "action": "auth",
+            "api_key": self.api_key,
+            "passphrase": self.passphrase,
+            "sign": signature,
+            "timestamp": timestamp,
+            "request_id": "my_request"
         }
         self.conn.send(data=json.dumps(req))
 
@@ -121,17 +148,33 @@ class FalconXWSClient:
         }
         self.conn.send(data=json.dumps(susbcription_request))
 
+    def unsusbcribe(self, base_token, quote_token):
+        unsusbcription_request = {
+            "base_token": base_token,
+            "quote_token": quote_token,
+            "request_id": "my_request_1",
+            "action": "unsubscribe"
+        }
+        self.conn.send(data=json.dumps(unsusbcription_request))
+
+    def fetch_data(self, type_of_data):
+        data_request = {
+            "request_type": type_of_data,
+            "request_id": "my_request_1",
+            "action": "data_request"
+        }
+        self.conn.send(data=json.dumps(data_request))
+
 
 if __name__ == "__main__":
-    host = "stream.falconx.io"
     path = "/price.tickers"
+    host = "stream.falconx.io"
     api_key = 'xxx'
     passphrase = "xxx"
     secret_key = "xxx"
-    
+
     connection_opts = ConnectionOpts(retry_on_error=True, num_retries=5, retry_delay=1)
 
-    fx_ws_client = FalconXWSClient(host=host,path=path, api_key=api_key, secret=secret_key, passphrase=passphrase, connection_opts=connection_opts)
+    fx_ws_client = FalconXWSClient(host=host, path=path, api_key=api_key, secret=secret_key, passphrase=passphrase, connection_opts=connection_opts, ssl=True)
 
     fx_ws_client.connect()
-    
